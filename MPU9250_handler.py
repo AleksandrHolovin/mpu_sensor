cfb918ce301b9a63f2525bb36f830e3a7860d386
Oleksandr Holovin <aleksandrswork61@gmail.com>
2024-08-09 16:18:53 +0300

    Remove accelerometer add compass

--- MPU9250_handler.py
+++ MPU9250_handler.py
@@ -1,113 +1,97 @@
-#Include the library files
 import RPi.GPIO as GPIO
-import smbus 
+import smbus
 from time import sleep
+import math
 
 # Setup GPIO pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-# Set the servo motor pin as output pin
-GPIO.setup(4,GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
-pwm = GPIO.PWM(4,50)
+pwm = GPIO.PWM(4, 50)
 pwm.start(0)
 
-#some MPU6050 Registers and their Address
-PWR_MGMT_1   = 0x6B
-SMPLRT_DIV   = 0x19
-CONFIG       = 0x1A
-GYRO_CONFIG  = 0x1B
-INT_ENABLE   = 0x38
-ACCEL_XOUT = 0x3B
-ACCEL_YOUT = 0x3D
-ACCEL_ZOUT = 0x3F
-GYRO_XOUT  = 0x43
-GYRO_YOUT  = 0x45
-GYRO_ZOUT  = 0x47
+# MPU6050 and HMC5883L Addresses
+MPU6050_ADDRESS = 0x68
+HMC5883L_ADDRESS = 0x1E
 
+# MPU6050 Registers
+PWR_MGMT_1 = 0x6B
+SMPLRT_DIV = 0x19
+CONFIG = 0x1A
+GYRO_CONFIG = 0x1B
+INT_ENABLE = 0x38
+GYRO_XOUT = 0x43
+GYRO_YOUT = 0x45
+GYRO_ZOUT = 0x47
 
-bus = smbus.SMBus(1) # or bus = smbus.SMBus(0) for older version boards
-Device_Address = 0x68 # MPU6050 device address
+# HMC5883L Registers
+CONFIG_A = 0x00
+MODE = 0x02
+DATAX0 = 0x03
+DATAX1 = 0x04
+DATAY0 = 0x05
+DATAY1 = 0x06
+DATAZ0 = 0x07
+DATAZ1 = 0x08
+
+bus = smbus.SMBus(1)
 
 def angle(Angle):
     duty = Angle / 18 + 2
-    GPIO.output(4,True)
+    GPIO.output(4, True)
     pwm.ChangeDutyCycle(duty)
-#     sleep(1)
-    GPIO.output(4,False)
-#     pwm.ChangeDutyCycle(0)
-    
-def setAngle():
-    angle(90)
+    sleep(0.1)
+    GPIO.output(4, False)
 
-def MPU_Init():
-    
-    #write to sample rate register
-    bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+def MPU6050_Init():
+    bus.write_byte_data(MPU6050_ADDRESS, SMPLRT_DIV, 7)
+    bus.write_byte_data(MPU6050_ADDRESS, PWR_MGMT_1, 1)
+    bus.write_byte_data(MPU6050_ADDRESS, CONFIG, 0)
+    bus.write_byte_data(MPU6050_ADDRESS, GYRO_CONFIG, 24)
+    bus.write_byte_data(MPU6050_ADDRESS, INT_ENABLE, 1)
 
-    #Write to power management register
-    bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+def HMC5883L_Init():
+    bus.write_byte_data(HMC5883L_ADDRESS, CONFIG_A, 0x70)
+    bus.write_byte_data(HMC5883L_ADDRESS, MODE, 0)
 
-    #Write to Configuration register
-    bus.write_byte_data(Device_Address, CONFIG, 0)
-
-    #Write to Gyro configuration register
-    bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-
-    #Write to interrupt enable register
-    bus.write_byte_data(Device_Address, INT_ENABLE, 1)
-
-def read_raw_data(addr):
-    #Accelero and Gyro value are 16-bit
-        high = bus.read_byte_data(Device_Address, addr)
-        low = bus.read_byte_data(Device_Address, addr+1)
-    
-        #concatenate higher and lower value
-        value = ((high << 8) | low)
-        
-        #to get signed value from mpu6050
-        if(value > 32768):
-                value = value - 65536
+def read_raw_data_16bit(addr, device_addr):
+    high = bus.read_byte_data(device_addr, addr)
+    low = bus.read_byte_data(device_addr, addr + 1)
+    value = (high << 8) | low
+    if value >= 0x8000:
+        return -((65535 - value) + 1)
+    else:
         return value
-    
 
-MPU_Init()
+MPU6050_Init()
+HMC5883L_Init()
 
 while True:
-    #Read Accelerometer raw value
-    acc_x = read_raw_data(ACCEL_XOUT)
-    acc_y = read_raw_data(ACCEL_YOUT)
-    acc_z = read_raw_data(ACCEL_ZOUT)
+    # Read Gyroscope raw values
+    gyro_x = read_raw_data_16bit(GYRO_XOUT, MPU6050_ADDRESS)
+    gyro_y = read_raw_data_16bit(GYRO_YOUT, MPU6050_ADDRESS)
+    gyro_z = read_raw_data_16bit(GYRO_ZOUT, MPU6050_ADDRESS)
 
-    #Read Gyroscope raw value
-    gyro_x = read_raw_data(GYRO_XOUT)
-    gyro_y = read_raw_data(GYRO_YOUT)
-    gyro_z = read_raw_data(GYRO_ZOUT)
+    Gx = gyro_x / 131.0
+    Gy = gyro_y / 131.0
+    Gz = gyro_z / 131.0
 
-    Ax = acc_x/16384.0
-    Ay = acc_y/16384.0 
-    Az = acc_z/16384.0
+    print(f"Gyroscope -> Gx: {Gx:.2f}, Gy: {Gy:.2f}, Gz: {Gz:.2f} degrees/s")
 
-    Gx = gyro_x/131.0
-    Gy = gyro_y/131.0
-    Gz = gyro_z/131.0
+    # Read Magnetometer raw values
+    x = read_raw_data_16bit(DATAX0, HMC5883L_ADDRESS)
+    y = read_raw_data_16bit(DATAY0, HMC5883L_ADDRESS)
+    z = read_raw_data_16bit(DATAZ0, HMC5883L_ADDRESS)
 
-# Uncomment below line to see the Accelerometer and Gyroscope values   
-#    print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
-       
-    in_min = 1
-    in_max = -1
-    out_min = 0
-    out_max = 180
-    
-#     setAngle() # Use this function to set the servo motor point
-    
-    # Convert accelerometer Y axis values from 0 to 180   
-    value = (Ay - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-    value = int(value)
-    print(value)
-    if value >= 0 and value <= 180:
-        # Write these values on the servo motor
-        angle(value) # Rotate the servo motor using the sensor values
-        sleep(0.08)
-        
+    heading = math.atan2(y, x) * (180 / math.pi)
+    if heading < 0:
+        heading += 360
+
+    print(f"Compass Heading: {heading:.2f} degrees")
+
+    # Map heading to servo motor angle (0-180 degrees)
+    angle_value = int(heading / 2)
+    angle(angle_value)
+
+    sleep(0.1)  # Adjust as needed
